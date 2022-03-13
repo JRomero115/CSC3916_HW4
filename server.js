7fb63ea9eb6f41d76f36468358a5794562b468ea
Janine Romero <janine.romero@ucdenver.edu>
2022-03-12 17:36:00 -0700

    commit mongo changes

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ router.post('/signup', function(req, res) {
         user.save(function(err){
             if (err) {
                 if (err.code == 11000)
-                    return res.json({ success: false, message: 'A user with that username already exists.'});
+                    return res.json({ success: false, msg: 'A user with that username already exists.'});
                 else
                     return res.json(err);
             }
