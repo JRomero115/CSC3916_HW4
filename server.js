5862492967333fd6f341ab37fa8e8a1cab84d13b
Janine Romero <janine.romero@ucdenver.edu>
2022-04-12 23:05:11 -0600

    update

--- server.js
+++ server.js
@@ -258,7 +258,7 @@ router.route('/reviews')
         if (!req.body.title || !req.body.nameReview || !req.body.quote || !req.body.rating) {
             res.json({success: false, msg: 'Please include a title, username, a quote, and a rating out of 5.'})
         } else {
-            Movie.findOne({ title: req.body.title }, function (err, movie) {
+            Movie.findOne({ title: req.body.title }, function (err, review) {
                 var review = new Review();
                 review.title = req.body.title;
                 review.nameReview = req.body.nameReview;
